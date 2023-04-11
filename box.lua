2097355e28c38f6de16b4e6b452361f0c964019c
ObsoletePoint <118140329+ObsoletePoint@users.noreply.github.com>
2023-04-11 08:56:45 -0300

    0.2.0.1
    Random spawning of boxes.

--- box.lua
+++ box.lua
@@ -2,6 +2,13 @@ Box = Object:extend()
 
 function Box:new()
 
+    self.width = 40
+    self.height = 40
+
+    self.x = math.random(0, love.graphics.getWidth() - self.width)
+    self.y = math.random(0, love.graphics.getHeight() - self.height)
+
+
 end
 
 function Box:update(dt)
@@ -9,5 +16,7 @@ function Box:update(dt)
 end
 
 function Box:draw()
+
+    love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
 
 end
--- main.lua
+++ main.lua
@@ -1,9 +1,22 @@
+local boxes = {}
+local maxBoxes = 10
+
+
 function love.load()
   
     Object = require "library/classic"
     require "player"
+    require "box"
+
+    math.randomseed(os.time())
 
     player = Player()
+
+    for i = 1, maxBoxes do
+
+        table.insert(boxes, Box())
+
+    end
 
 end
 
@@ -11,10 +24,32 @@ function love.update(dt)
 
     player:update(dt)
 
+    for i, box in ipairs(boxes) do
+
+        box:update(dt)
+
+    end
+
 end
 
 function love.draw()
 
     player:draw()
+
+    for i, box in ipairs(boxes) do
+
+        box:draw()
+
+    end
+
+end
+
+function addBox()
+
+    if #boxes < maxBoxes then
+
+        table.insert(boxes, Box())
+
+    end
 
 end
